77df42fa0c4d9feebd39e6919bffb77a6bf320da
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-14 21:14:40 +0100

    update header file

--- monty.h
+++ monty.h
@@ -55,4 +55,5 @@ void func_call(char **tokens, stack_t **stack);
 /* functions_0.c */
 void _pall(stack_t **stack, __attribute__((unused)) unsigned int line_number);
 void _push(stack_t **stack, __attribute__((unused)) unsigned int line_number);
+void _pint(stack_t **stack, __attribute__((unused)) unsigned int line_number);
 #endif /* MONTY_H */
