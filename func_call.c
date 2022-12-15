34b752acdbad039be2c3cc05d773adccc2d477ea
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-15 15:13:40 +0100

    [add] functions to subtract and divide on stack && refactor arithmetic

--- func_call.c
+++ func_call.c
@@ -11,7 +11,8 @@ void func_call(char **tokens, stack_t **stack)
 	instruction_t func[] = {
 			{"pall", _pall}, {"push", _push}, {"pint", _pint},
 			{"pop", _pop}, {"swap", _swap}, {"nop", NULL},
-			{"add", _add}, {NULL, NULL}};
+			{"add", _add}, {"sub", _sub}, {"div", _div},
+			{NULL, NULL}};
 	int idx;
 
 	for (idx = 0; func[idx].opcode != NULL; idx++)
@@ -34,44 +35,6 @@ void func_call(char **tokens, stack_t **stack)
 			dprintf(STDERR_FILENO,
 					"L%u: unknown instruction %s\n",
 					line_number, tokens[0]);
-			exit(EXIT_FAILURE);
-		}
-	}
-}
-/**
- * check_valid_digit - checks if the value is a digit or
- * if a value is passed
- * @tokens: array od pointers to function opcode and value
- * @stack: pointer to head of stack linked list
- * Return: nothing
- */
-void check_valid_digit(char **tokens, stack_t **stack)
-{
-	int idx = 0;
-
-	if (!(tokens[1][idx]))
-	{
-		dprintf(STDERR_FILENO, "L%u: usage: push integer\n",
-				line_number);
-		if (*stack)
-			free_stack_list(stack);
-		free(tokens);
-		exit(EXIT_FAILURE);
-	}
-
-	for ( ; tokens[1][idx]; idx++)
-	{
-		if (tokens[1][idx] == '-' && idx == 0)
-		{
-			continue;
-		}
-		if (isdigit(tokens[1][idx]) == 0)
-		{
-			dprintf(STDERR_FILENO, "L%u: usage: push integer\n",
-					line_number);
-			if (*stack)
-				free_stack_list(stack);
-			free(tokens);
 			exit(EXIT_FAILURE);
 		}
 	}
--- functions_1.c
+++ functions_1.c
@@ -9,7 +9,7 @@
  */
 void _add(stack_t **stack, __attribute__((unused)) unsigned int line_number)
 {
-	int count, addn_res;
+	int count;
 	stack_t *temp, *f_node, *s_node;
 
 	if (!stack || !(*stack))
@@ -33,7 +33,85 @@ void _add(stack_t **stack, __attribute__((unused)) unsigned int line_number)
 	f_node = *stack;
 	s_node = (*stack)->next;
 
-	addn_res = s_node->n + f_node->n;
-	s_node->n = addn_res;
+	s_node->n += f_node->n;
+	_pop(stack, line_number);
+}
+/**
+ * _sub - subtracts the first two data on the stack and deletes the
+ * first node, making the second node storage for the result, and
+ * top of the stack
+ * @stack: pointer to head/top of linked list/stack
+ * @line_number: see _push for reference (unused argument)
+ * Return: nothing
+ */
+void _sub(stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+	int count;
+	stack_t *temp, *f_node, *s_node;
+
+	if (!stack || !(*stack))
+	{
+		dprintf(STDERR_FILENO, "L%u: can't sub, stack too short\n",
+				line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	for (count = 0; temp; count++)
+		temp = temp->next;
+
+	if (count < 2)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't sub, stack too short\n",
+				line_number);
+		if (*stack)
+			free_stack_list(stack);
+		exit(EXIT_FAILURE);
+	}
+	f_node = *stack;
+	s_node = (*stack)->next;
+
+	s_node->n -= f_node->n;
+	_pop(stack, line_number);
+}
+/**
+ * _div - adds the first two data on the stack and deletes the
+ * first node, making the second node storage for the result, and
+ * top of the stack
+ * @stack: pointer to head/top of linked list/stack
+ * @line_number: see _push for reference (unused argument)
+ * Return: nothing
+ */
+void _div(stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+	int count;
+	stack_t *temp, *f_node, *s_node;
+
+	if (!stack || !(*stack))
+	{
+		dprintf(STDERR_FILENO, "L%u: can't div, stack too short\n",
+				line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	for (count = 0; temp; count++)
+		temp = temp->next;
+
+	if (count < 2)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't div, stack too short\n",
+				line_number);
+		if (*stack)
+			free_stack_list(stack);
+		exit(EXIT_FAILURE);
+	}
+	f_node = *stack;
+	s_node = (*stack)->next;
+	if (f_node->n == 0)
+	{
+		dprintf(STDERR_FILENO, "L%u: division by zero\n", line_number);
+		free_stack_list(stack);
+		exit(EXIT_FAILURE);
+	}
+	s_node->n /= f_node->n;
 	_pop(stack, line_number);
 }
