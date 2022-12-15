5d4a310e795e625b22f63be61439d93b182e429a
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-15 15:32:45 +0100

    update header file and refactor prototypes

--- monty.h
+++ monty.h
@@ -53,12 +53,15 @@ void check_valid_digit(char **tokens, stack_t **stack);
 void func_call(char **tokens, stack_t **stack);
 
 /* functions_0.c */
-void _pall(stack_t **stack, __attribute__((unused)) unsigned int line_number);
-void _push(stack_t **stack, __attribute__((unused)) unsigned int line_number);
-void _pint(stack_t **stack, __attribute__((unused)) unsigned int line_number);
-void _pop(stack_t **stack, __attribute__((unused)) unsigned int line_number);
-void _swap(stack_t **stack, __attribute__((unused)) unsigned int line_number);
-void _add(stack_t **stack, __attribute__((unused)) unsigned int line_number);
-void _sub(stack_t **stack, __attribute__((unused)) unsigned int line_number);
-void _div(stack_t **stack, __attribute__((unused)) unsigned int line_number);
+void _pall(stack_t **stack, unsigned int line_number);
+void _push(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
+void _sub(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
+
 #endif /* MONTY_H */
