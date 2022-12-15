70c1d4dc82771403829e66da9cdab7a09920e91e
Taiwo Babalola <babalolataiwop@gmail.com>
2022-12-15 15:13:58 +0100

    update header

--- monty.h
+++ monty.h
@@ -59,4 +59,6 @@ void _pint(stack_t **stack, __attribute__((unused)) unsigned int line_number);
 void _pop(stack_t **stack, __attribute__((unused)) unsigned int line_number);
 void _swap(stack_t **stack, __attribute__((unused)) unsigned int line_number);
 void _add(stack_t **stack, __attribute__((unused)) unsigned int line_number);
+void _sub(stack_t **stack, __attribute__((unused)) unsigned int line_number);
+void _div(stack_t **stack, __attribute__((unused)) unsigned int line_number);
 #endif /* MONTY_H */
